f64404de9a2d81dff0186f69347711b0073e0e1b
kamranhasan <kamranhasann@gmail.com>
2023-09-23 17:23:23 +0500

    schema updates

--- books/models.py
+++ books/models.py
@@ -11,8 +11,8 @@ class Book(models.Model):
 class Section(models.Model):
     title = models.CharField(max_length=255)
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='sections')
-    parent_section = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='child_sections')
-
+    
 class Subsection(models.Model):
     title = models.CharField(max_length=255)
     section = models.ForeignKey(Section, on_delete=models.CASCADE, related_name='subsections')
+    parent_subsection = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='child_sections')
